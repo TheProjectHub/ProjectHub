a3e808ab6764e5d25f54fe905ab08bf52e5fad43
Shaham Noorani <shahamnoorani@gmail.com>
2020-07-12 14:45:11 -0500

    showing how to use vue binds and added bootstrap\!

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+// Bootstrap
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 Vue.config.productionTip = false;
 
 new Vue({
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button type="button" class="btn btn-primary" style="margin-bottom: 20px"
+    v-on:click="greet">Click Me!</button>
   </div>
 </template>
 
@@ -13,6 +15,12 @@ export default {
   name: 'Home',
   components: {
     HelloWorld,
+  },
+  methods: {
+    greet() {
+      // eslint-disable-next-line
+      alert('Wassup!');
+    },
   },
 };
 </script>
