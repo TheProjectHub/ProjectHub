e51a755b03893aac2552262ffb57f1e2e442527f
Jacob <jacobt1206@gmail.com>
2020-07-12 15:55:02 -0500

    Add style tag

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -12,7 +11,9 @@ import HelloWorld from "@/components/HelloWorld.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 };
 </script>
+
+<style lang="stylus"></style>
