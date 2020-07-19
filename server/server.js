474e8553243e7c4176fa670180708996edab1540
Shaham Noorani <shahamnoorani@gmail.com>
2020-07-19 13:52:14 -0500

    updated CORS

--- server/server.js
+++ server/server.js
@@ -9,6 +9,15 @@ app.use(bodyParser.json());
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Projectly." });
