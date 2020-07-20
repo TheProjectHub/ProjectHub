7af75e97f061f12c25da1aede85384a124b5b85e
Shaham Noorani <shahamnoorani@gmail.com>
2020-07-20 02:41:24 -0500

    front end works :D

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,32 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
 // Bootstrap
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { domain, clientId } from '../auth_config.json';
+
+// Import the plugin here
+import { Auth0Plugin } from './auth';
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: (appState) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  },
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
