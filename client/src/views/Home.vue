93fec588017c1becd40d0fd498ca8be0297f80a7
Shaham Noorani <shahamnoorani@gmail.com>
2020-07-22 14:42:42 -0500

    cleaning up code and fixing logo import

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,8 +4,8 @@
     <title>Welcome to Projectly</title>
   </head>
   <body>
-    <div class="home">
-      <HelloWorld msg="Howdy" />
+    <div class='home'>
+      <HelloWorld msg='Howdy' />
       <Brandname />
     </div>
   </body>
@@ -14,15 +14,15 @@
 
 <script>
 // @ is an alias to /src
-import Brandname from "../components/Brandname.vue";
-import HelloWorld from "../components/HelloWorld.vue";
+import Brandname from '../components/Brandname.vue';
+import HelloWorld from '../components/HelloWorld.vue';
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
     Brandname,
-    HelloWorld
-  }
+    HelloWorld,
+  },
 };
 </script>
 
