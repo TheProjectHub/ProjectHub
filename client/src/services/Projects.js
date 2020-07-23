5b4c779f9419f65bb6bbbf207b61aa9942a8af67
Shaham Noorani <shahamnoorani@gmail.com>
2020-07-23 09:46:10 -0500

    updated project and convo services

--- client/src/services/Projects.js
+++ client/src/services/Projects.js
@@ -2,15 +2,27 @@ import http from "../http-common";
 
 class Projects {
   get(id) {
-    return http.get(`/projects/${id}`);
+    return http.get(`/projects/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
   }
 
   create(data) {
-    return http.post("/projects", data);
+    return http.post('/projects', data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
   }
 
   update(id, data) {
-    return http.put(`/projects/${id}`, data);
+    return http.put(`/projects/${id}`, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
   }
 }
 
