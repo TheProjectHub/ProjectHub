aa4cdb458610ed7afc275901516d1927b4a0b0f9
Jacob <jacobt1206@gmail.com>
2020-07-26 12:39:35 -0500

    nothing

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,28 +1,28 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
-import About from '../views/About.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 
-import { authGuard } from '../auth/authGuard';
+import { authGuard } from "../auth/authGuard";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     component: About,
     beforeEnter: authGuard,
   },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes,
 });
