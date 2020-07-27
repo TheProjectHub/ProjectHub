12e2ffd57de07bf4ffed5028625961b0dfa4421f
Shaham Noorani <shahamnoorani@gmail.com>
2020-07-27 17:44:50 -0500

    i think its good

--- client/src/components/Splash.vue
+++ client/src/components/Splash.vue
@@ -14,9 +14,15 @@
     <div class="row">
       <div class="column">
         <h1>
-          Find Your Team with <span style="font-family: Comfortaa, cursive; color: black;"
-            >Projectly</span
+          Find Your Team with
+          <span
+            style="background-color: rgba(255, 255, 255, 0.5);
+            color: transparent; border-radius: 25px;"
           >
+            <span style="font-family: Comfortaa, cursive; padding: 10px" class="gradient"
+              >Projectly</span
+            >
+          </span>
         </h1>
       </div>
       <div class="column">
@@ -146,8 +152,21 @@ button {
 }
 
 .gradient {
-  background: -webkit-linear-gradient(45deg, rgb(179, 33, 33), rgb(6, 84, 107));
+  background: linear-gradient(-45deg, #b721ff, #23a6d5, #500000) fixed;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+  animation: gradient 5s ease infinite;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
 }
 </style>
