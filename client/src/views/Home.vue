dddd339c786b2384cfb4b31eeb70a4901cb28a56
Shaham Noorani <shahamnoorani@gmail.com>
2020-07-27 18:13:08 -0500

    purple to yellow

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -24,7 +24,7 @@ export default {
 
 <style>
 body {
-  background: linear-gradient(-45deg, #b721ff, #23a6d5, #500000) fixed;
+  background: linear-gradient(-45deg, #fbff21, #23a6d5, #500000) fixed;
   background-size: 400% 400%;
   animation: gradient 45s ease infinite;
 }
