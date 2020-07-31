0e4b66557559151f89ec8014c5bb3cba12625eb9
Jacob <jacobt1206@gmail.com>
2020-07-31 18:15:04 -0500

    updated profile page

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,29 +1,43 @@
 <template>
 <html>
-  <head>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-    />
-  </head>
+  <head></head>
   <body>
-    <h2 style="text-align:center">{{this.user.first_name}}'s Card</h2>
-
-    <div class="card">
-      <img src="../assets/logo.png" alt="Vue logo" style="width:100%" />
-      <h1>{{this.user.first_name}}</h1>
-      <h1>{{this.user.last_name}}</h1>
-      <h2>{{this.user.location}}</h2>
-      <p class="title">{{this.user.bio}}</p>
-      <ul v-for="skill in this.user.skills" :key="skill">{{skills}}</ul>
-      <div style="margin: 24px 0;">
-        <p v-if="!this.user.looking_for_project">Actively looking for projects!</p>
-        <h2>Project affiliations</h2>
-        <ul
-          style="list-style: none"
-          v-for="project in this.projects"
-          :key="project"
-        >{{project.name}}</ul>
+    <div class="profile">
+      <div class="toprow">
+        <div class="PP">
+          <img alt="Vue logo" src="../assets/logo.png" />
+        </div>
+        <div class="user">
+          <div class="name">
+            <h2>{{this.user.first_name}} {{this.user.last_name}}</h2>
+            <h3 v-if="!this.user.looking_for_project">is looking for a project!</h3>
+          </div>
+          <p>
+            <strong>{{this.user.age}} yrs old</strong>
+          </p>
+          <p>
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            {{this.user.location}}
+          </p>
+          <a href="/conversations" type="button" class="msg-btn">
+            <i class="fa fa-envelope-o" aria-hidden="true"></i>Send Message
+          </a>
+          <p>{{this.user.bio}}</p>
+        </div>
+      </div>
+      <div class="bottomrow">
+        <div class="skills">
+          <h3>I have skills in:</h3>
+          <ul style="list-style:none" v-for="skill in this.user.skills" :key="skill">{{skill}}</ul>
+        </div>
+        <div class="projects">
+          <h3>I am affiliated with these projects:</h3>
+          <ul
+            style="list-style:none"
+            v-for="project in this.user.projects"
+            :key="project"
+          >{{project.name}}</ul>
+        </div>
       </div>
     </div>
   </body>
@@ -87,20 +101,72 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-body {
-  background-color: #d7d7d7;
+.profile {
+  width: 80%;
+  padding: 3px;
+  margin-right: auto;
+  margin-left: auto;
+  margin-top: 3%;
 }
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  max-width: 350px;
-  margin: auto;
+.toprow {
+  height: 40%;
+  width: 100%;
+}
+.PP {
+  float: left;
+  position: relative;
+  width: 25%;
+  height: 80%;
+  margin-top: 8px;
+}
+.PP img {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  margin-top: auto;
+}
+.user {
+  float: left;
+  width: 70%;
+  height: 80;
+}
+.bottomrow {
+  height: 40%;
+  width: 100%;
+}
+.skills {
+  float: left;
+  position: relative;
+  width: 50%;
+  height: 80%;
+  margin-top: 8px;
   text-align: center;
-  font-family: arial;
 }
-
-.title {
-  color: grey;
-  font-size: 18px;
+.projects {
+  float: left;
+  position: relative;
+  width: 50%;
+  height: 80%;
+  margin-top: 8px;
+  text-align: center;
+}
+.msg-btn {
+  background: #fff;
+  padding: 5px 11px;
+  color: black;
+  text-decoration: none;
+  font-size: 13px;
+}
+.msg-btn i {
+  padding: 0;
+  color: black;
+  margin-right: 7px;
+}
+h1,
+p,
+h2,
+h3 {
+  color: white;
 }
 </style>
 
