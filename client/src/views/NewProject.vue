3fc4af5a6c74d344c02fdbc78365540dcb978f93
Joey Huang <joeyhuang@Razer-Blade.local>
2020-08-06 14:33:25 -0500

    added a click.prevent, more work needed however, and font and link array

--- client/src/views/NewProject.vue
+++ client/src/views/NewProject.vue
@@ -8,36 +8,37 @@
     </head>
     <section class="main">
       <form>
-        <h1 class="title is-1">
+        <h1 style="font-family: Comfortaa, cursive; padding: 10px" class="title is-1">
           New Project
         </h1>
         <div class="field">
-          <label class="label">Project Name:</label>
+          <label class="label" style="font-family: Comfortaa, cursive">Project Name:</label>
           <div class="control">
             <input class="input" type="text" size = "40" v-model="form.projectName" />
           </div>
         </div>
         <div class="field">
-          <label class="label">Description:</label>
+          <label class="label" style="font-family: Comfortaa, cursive">Description:</label>
           <div class="control">
             <textarea rows="4" cols="50" v-model="form.description"></textarea>
           </div>
         </div>
         <div class="field">
-          <label class="label">Links:</label>
+          <label class="label" style="font-family: Comfortaa, cursive">Links:</label>
           <div class="control">
             <input class="input" type="text" size = "75" v-model="form.links" />
           </div>
         </div>
         <div class="field">
           <label>
-            <input type="checkbox" value="yes" v-model="form.lookingForNewMembers" />
+            <input type="checkbox" value="true" v-model="form.lookingForNewMembers" />
             Looking for New Members?
           </label>
         </div>
         <input
           class="btn btn-primary margin-bottom"
           type="submit"
+          @click.prevent="preventSubmit"
           @click="submitProject"
         />
       </form>
@@ -57,7 +58,7 @@ export default {
         projectName: '',
         description: '',
         links: '',
-        lookingForNewMembers: ['yes'],
+        lookingForNewMembers: ['true'],
       },
     };
   },
@@ -75,6 +76,14 @@ export default {
     isNameTaken(name) {
       return this.takenProjectNames.includes(name);
     },
+    async preventSubmit() {
+      if (this.form.projectName === '') {
+        this.showSubmitFeedback = true;
+        setTimeout(() => {
+          this.showSubmitFeedback = false;
+        }, 3000);
+      }
+    },
     async submitProject() {
       if (this.isNameTaken(this.form.projectName) && this.form.name) {
         alert(`The name: ${this.form.projectName} is taken!`); // eslint-disable-line
@@ -83,7 +92,7 @@ export default {
         const project = {
           members: JSON.stringify([this.$store.state.currentUser.id]),
           name: this.form.projectName,
-          links: this.form.links,
+          links: JSON.stringify(this.form.links.split(' ')),
           description: this.form.description,
           looking_for_new_members: this.form.lookingForNewMembers.length,
           search_filters: '[]',
@@ -113,17 +122,17 @@ ul {
   color: white;
 }
 .margin-bottom {
-  margin-bottom: 15px;
+  margin-bottom: 15vh;
 }
 .row {
   max-width: 83vw;
   margin: auto;
 }
 h1{
-  font-size: 50px;
+  font-size: 10vh;
 }
 h3 {
-  margin: 40px 0 0;
+  margin: 40vh 0 0;
 }
 ul {
   list-style-type: none;
@@ -131,10 +140,11 @@ ul {
 }
 li {
   display: inline-block;
-  margin: 0 10px;
+  margin: 0 10vh;
 }
 .label {
-  font-size: 30px;
+  font-size: 5vh;
+  margin: 1vh;
 }
 a {
   color: #42b983;
